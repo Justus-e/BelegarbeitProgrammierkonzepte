7765a266e85ff5af5eb31b098120624f6d08d05f
Justus Ernst <justus.ernst@mercedes-benz.com>
2022-11-30 11:29:47 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+int main()
+{
+    std::string image_path = "/Users/ernsjus/Dev/openMPITest/images/animal/1.kitten_small.jpg";
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if(img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    return 0;
+}
